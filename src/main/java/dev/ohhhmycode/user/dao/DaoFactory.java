da8575a1814c07e402e26e31f54b9af8c20cd8f8
김동욱 <dongwook_@a2dcorp.co.kr>
2022-06-28 19:16:40 +0900

    1.5 스프링의 IOC

--- src/main/java/dev/ohhhmycode/user/dao/DaoFactory.java
+++ src/main/java/dev/ohhhmycode/user/dao/DaoFactory.java
@@ -1,19 +1,23 @@
 package dev.ohhhmycode.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author 김동욱, dongwook_@a2dcorp.co.kr
  * @version 1.0
  * @since 2022-06-23
  */
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = connectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        return userDao;
+        return new UserDao(connectionMaker());
     }
 
-    private ConnectionMaker connectionMaker() {
+    @Bean
+    public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
 
--- src/main/java/dev/ohhhmycode/user/dao/UserDaoTest.java
+++ src/main/java/dev/ohhhmycode/user/dao/UserDaoTest.java
@@ -2,12 +2,15 @@ package dev.ohhhmycode.user.dao;
 
 import dev.ohhhmycode.user.domain.User;
 import java.sql.SQLException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("ohhhmycode");
