a30254fda511b29b48ffc7ebe9189a74ff295c38
김동욱 <dongwook_@a2dcorp.co.kr>
2022-07-06 18:41:02 +0900

    2.2 UserDaoTest 개선

---
+++ src/main/java/dev/ohhhmycode/user/dao/TestMain.java
@@ -0,0 +1,16 @@
+package dev.ohhhmycode.user.dao;
+
+import org.junit.runner.JUnitCore;
+
+/**
+ * @author 김동욱, dongwook_@a2dcorp.co.kr
+ * @version 1.0
+ * @since 2022-07-06
+ */
+public class TestMain {
+
+    public static void main (String[] args) {
+        JUnitCore.main("dev.ohhhmycode.user.dao.UserDaoTest");
+    }
+
+}
--- src/main/java/dev/ohhhmycode/user/dao/UserDaoTest.java
+++ src/main/java/dev/ohhhmycode/user/dao/UserDaoTest.java
@@ -1,39 +1,33 @@
 package dev.ohhhmycode.user.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import dev.ohhhmycode.user.domain.User;
 import java.sql.SQLException;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class UserDaoTest {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("ohhhmycode");
-        user.setName("Dongwook");
-        user.setPassword("studying..");
+        user.setId("dongwook");
+        user.setName("김동욱");
+        user.setPassword("1234");
 
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
-
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        if (!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (name)");
-        }
-        else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        }
-        else {
-            System.out.println("조회 테스트 성공");
-        }
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
     }
 
 }
