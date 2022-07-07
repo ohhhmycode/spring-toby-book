1af03f9473f10e26d641d724d48957c9b61ce8ef
김동욱 <dongwook_@a2dcorp.co.kr>
2022-07-07 18:06:39 +0900

    getCount() 테스트, addAndGet() 테스트 보완

--- src/main/java/dev/ohhhmycode/user/dao/UserDaoTest.java
+++ src/main/java/dev/ohhhmycode/user/dao/UserDaoTest.java
@@ -17,21 +17,46 @@ public class UserDaoTest {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        User user1 = new User("tester01", "테스터01", "t1-1234");
+        User user2 = new User("tester02", "테스터02", "t2-1234");
+
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
-        User user = new User();
-        user.setId("dongwook");
-        user.setName("김동욱");
-        user.setPassword("1234");
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        dao.add(user);
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
+
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
+    }
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        User user1 = new User("tester01", "테스터01", "t1-1234");
+        User user2 = new User("tester02", "테스터02", "t2-1234");
+        User user3 = new User("tester03", "테스터03", "t3-1234");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
         assertThat(dao.getCount(), is(1));
 
-        User user2 = dao.get(user.getId());
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
     }
 
 }
