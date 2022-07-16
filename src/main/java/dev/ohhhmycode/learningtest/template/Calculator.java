bd56979bfc9cea376c4e61d7c0c1156bbf091188
ohhhmycode <ohhhmycode@gmail.com>
2022-07-16 15:11:55 +0900

    3.5.3 calcSum 메서드에 try, catch, finally 적용

--- src/main/java/dev/ohhhmycode/learningtest/template/Calculator.java
+++ src/main/java/dev/ohhhmycode/learningtest/template/Calculator.java
@@ -7,16 +7,28 @@ import java.io.IOException;
 
 public class Calculator {
     public int calcCum(String filePath) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filePath));
-        Integer sum = 0;
-        String line = null;
+        BufferedReader br = null;
 
-        while ((line = br.readLine()) != null) {
-            sum += Integer.valueOf(line);
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            Integer sum = 0;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+            return sum;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
-
-        br.close();
-        return sum;
     }
 
 }
