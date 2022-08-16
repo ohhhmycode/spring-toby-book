bb1c26e45e6555dde14e5f2cf062b5aa076f18c9
ohhhmycode <ohhhmycode@gmail.com>
2022-08-16 23:39:53 +0900

    5.1.3 upgradeLevels() 메서드, 테스트

--- src/main/java/dev/ohhhmycode/user/service/UserService.java
+++ src/main/java/dev/ohhhmycode/user/service/UserService.java
@@ -2,11 +2,40 @@ package dev.ohhhmycode.user.service;
 
 
 import dev.ohhhmycode.user.dao.UserDao;
+import dev.ohhhmycode.user.domain.Level;
+import dev.ohhhmycode.user.domain.User;
+
+import java.util.List;
 
 public class UserService {
     UserDao userDao;
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            boolean changed = false;
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            }
+            else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            }
+            else if (user.getLevel() == Level.GOLD) {
+                changed = false;
+            }
+            else {
+                changed = false;
+            }
+
+            if (changed) {
+                userDao.update(user);
+            }
+        }
     }
 }
--- src/main/java/dev/ohhhmycode/user/service/UserServiceTest.java
+++ src/main/java/dev/ohhhmycode/user/service/UserServiceTest.java
@@ -1,10 +1,17 @@
 package dev.ohhhmycode.user.service;
 
+import dev.ohhhmycode.user.dao.UserDao;
+import dev.ohhhmycode.user.domain.Level;
+import dev.ohhhmycode.user.domain.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -13,11 +20,48 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserServiceTest {
+
+    @Autowired
+    UserDao userDao;
+
     @Autowired
     UserService userService;
+    List<User> users;
+
+    @Before
+    public void setUp() {
+        users = Arrays.asList(
+                new User("user-1", "User01", "p1", Level.BASIC, 49, 0),
+                new User("user-2", "User02", "p2", Level.BASIC, 50, 0),
+                new User("user-3", "User03", "p3", Level.SILVER, 60, 29),
+                new User("user-4", "User04", "p4", Level.SILVER, 60, 30),
+                new User("user-5", "User05", "p5", Level.GOLD, 100, 100)
+        );
+    }
 
     @Test
     public void bean() {
         assertThat(this.userService, is(notNullValue()));
+    }
+
+    @Test
+    public void upgradeLevels() {
+        userDao.deleteAll();
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel(), is(expectedLevel));
     }
 }
