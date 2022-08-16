c76b5e8566e85c6791592af1e3169d347f7b9d81
ohhhmycode <ohhhmycode@gmail.com>
2022-08-16 23:51:36 +0900

    5.1.4 UserService.add() 작성, 테스트

--- src/main/java/dev/ohhhmycode/user/service/UserService.java
+++ src/main/java/dev/ohhhmycode/user/service/UserService.java
@@ -38,4 +38,12 @@ public class UserService {
             }
         }
     }
+
+    public void add(User user) {
+        if (user.getLevel() == null) {
+            user.setLevel(Level.BASIC);
+        }
+
+        userDao.add(user);
+    }
 }
