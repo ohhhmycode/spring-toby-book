73c84b72fd127b9a2c820e9ee45847c9e17efee4
ohhhmycode <ohhhmycode@gmail.com>
2022-08-17 21:54:24 +0900

    5.1.5 코드 개선 upgradeLevels() 리팩토링

--- src/main/java/dev/ohhhmycode/user/domain/User.java
+++ src/main/java/dev/ohhhmycode/user/domain/User.java
@@ -19,6 +19,14 @@ public class User {
         this.login = login;
         this.recommend = recommend;
     }
+
+    public void upgradeLevel() {
+        Level nextLevel = this.level.nextLevel();
+        if (nextLevel == null) {
+            throw new IllegalStateException(this.level + "은 업그레이드가 불가능합니다.");
+        }
+        this.level = nextLevel;
+    }
     public String getId() {
         return id;
     }
