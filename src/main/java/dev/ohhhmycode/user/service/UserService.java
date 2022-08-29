d6d681db6bf5abc43536ad63c891abd0936e6040
ohhhmycode <ohhhmycode@gmail.com>
2022-08-29 21:14:06 +0900

    6.1.1 메소드 분리

--- src/main/java/dev/ohhhmycode/user/service/UserService.java
+++ src/main/java/dev/ohhhmycode/user/service/UserService.java
@@ -38,16 +38,20 @@ public class UserService {
             new DefaultTransactionDefinition());
 
         try {
-            List<User> users = userDao.getAll();
-            for (User user : users) {
-                if (canUpgradeLevel(user)) {
-                    upgradeLevel(user);
-                }
-            }
+            upgradeLevelsInternal();
             this.transactionManager.commit(status);
         } catch (Exception e) {
             this.transactionManager.rollback(status);
             throw e;
+        }
+    }
+
+    private void upgradeLevelsInternal() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+            }
         }
     }
 
